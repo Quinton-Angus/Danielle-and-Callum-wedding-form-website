ad2a2a055402b1df75b9bde66cddd0dc74deb583
Quinton Angus <quinton@quintondev.com>
2026-09-04 22:20:20 +0100

    commit

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const hamburgerMenuOpenBtn = document.getElementById("headerMobileHamburger")
 const headerMobile = document.getElementById("headerMobile")
+const headerMobileHamburgerClose = document.getElementById("headerMobileHamburgerClose")
 
 hamburgerMenuOpenBtn.addEventListener('click', () => {
     console.log("ok")
-    headerMobile.classList.remove("headerMobileDeactive")
+    headerMobile.classList.remove("headerMobileDeactivated")
     headerMobile.classList.add("headerMobileActivated")
+    console.log(headerMobile.classList)
+})
+
+headerMobileHamburgerClose.addEventListener("click", () => {
+    headerMobile.classList.remove("headerMobileActivated")
+    headerMobile.classList.add("headerMobileDeactivated")
 })
