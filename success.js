0ff05b1b0aca9c915ad848465ec0095e4b3e8263
Quinton Angus <quinton@quintondev.com>
2026-09-05 17:59:06 +0100

    Fix mobile hamburger menu script

--- success.js
+++ success.js
@@ -2,14 +2,12 @@ const hamburgerMenuOpenBtn = document.getElementById("headerMobileHamburger")
 const headerMobile = document.getElementById("headerMobile")
 const headerMobileHamburgerClose = document.getElementById("headerMobileHamburgerClose")
 
-hamburgerMenuOpenBtn.addEventListener('click', () => {
-    console.log("ok")
-    headerMobile.classList.remove("headerMobileDeactivated")
-    headerMobile.classList.add("headerMobileActivated")
-    console.log(headerMobile.classList)
+hamburgerMenuOpenBtn?.addEventListener("click", () => {
+    headerMobile?.classList.remove("headerMobileDeactivated")
+    headerMobile?.classList.add("headerMobileActivated")
 })
 
-headerMobileHamburgerClose.addEventListener("click", () => {
-    headerMobile.classList.remove("headerMobileActivated")
-    headerMobile.classList.add("headerMobileDeactivated")
+headerMobileHamburgerClose?.addEventListener("click", () => {
+    headerMobile?.classList.remove("headerMobileActivated")
+    headerMobile?.classList.add("headerMobileDeactivated")
 })
